6bcd7be35852b69666d2c36dd1feee92c25930b5
Meng Li <li.meng@neuralgalaxy.com>
2020-07-20 20:03:53 +0800

    update

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 mkdir build && cd build  && \
-cmake -DENABLE_UI=false -DENABLE_SCRIPTING=false -DUNIX_STRUCTURE=0 -DCMAKE_INSTALL_PREFIX="${HOME}/obs-studio-portable" ..  && \
+cmake -DENABLE_UI=0 -DENABLE_SCRIPTING=0 -DUNIX_STRUCTURE=0 -DCMAKE_INSTALL_PREFIX="${HOME}/obs-studio-portable" ..  && \
 make -j1 && make install
